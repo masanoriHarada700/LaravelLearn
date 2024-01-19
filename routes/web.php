52c2f29a09f77c7ff6d3c2db495852f8d24c38dc
moroken700 <unconfigured@null.spigotmc.org>
2024-01-19 16:42:50 +0900

    route名変更

--- routes/web.php
+++ routes/web.php
@@ -52,4 +52,4 @@ Route::delete('/profile', [ProfileController::class, 'destroy'])
     ->name('profile.destroy');
 
 Route::patch('/profile', [ProfileController::class, 'update'])
-    ->name('profile.update');
+    ->name('profile.edit');
